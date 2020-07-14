756840e2e43265e642cf07e84aeac99f91a7ea16
Aditya Prakash <p.aditya.198@gmail.com>
2020-07-14 22:08:02 +0530

    full screen mode added

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
-const fullScreen=document.getElementById('full-screen')
+const fullScreen = document.getElementById("full-screen");
+const body = document.querySelector("body");
 const button = play.childNodes[1];
 
 const toggleVideo = () => {
@@ -29,8 +30,6 @@ video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
 play.addEventListener("click", toggleVideo);
 
-
-
 video.addEventListener("timeupdate", (event) => {
   progress.value = (video.currentTime / video.duration) * 100;
   let mins = Math.floor(event.target.currentTime / 60);
@@ -49,9 +48,21 @@ stop.addEventListener("click", () => {
   video.pause();
 });
 
+progress.addEventListener("change", (event) => {
+  video.currentTime = (+event.target.value * video.duration) / 100;
+});
 
-progress.addEventListener('change',(event)=>{
-  video.currentTime=(+event.target.value*video.duration)/100
+fullScreen.addEventListener("click", () => {
+  body.requestFullscreen();
+});
+
+body.addEventListener("fullscreenchange",(event)=>{
+  console.log(event)
+  if(document.webkitIsFullScreen){
+    video.style.width = screen.width + "px";
+    video.style.height = screen.height + "px";
+  }else{
+    video.style.height='';
+    video.style.width="60%"
+  }
 })
-
-//TODO : fullscreen mode
